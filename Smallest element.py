7c5bc4ccc13192b197ed324ff8dc4614e903a7d7
Yadnesh Madhukar Rane <102983422+Poneglyph795@users.noreply.github.com>
2025-01-20 22:05:24 +0530

    Add files via upload

---
+++ Smallest element.py
@@ -0,0 +1,29 @@
+
+
+def Smallest_element(array):
+     Smallest_number=array[0]
+     Smallest_index=0
+
+     for i in range(1 , len(array)):
+      if(array[i]<Smallest_index):
+               Smallest_number=array[i]
+               Smallest_index=i
+     return Smallest_index,Smallest_number
+
+
+
+## Use fucntion to write selction sort:
+def selectionSort(array):
+     New_array=[]
+     for i in range(len(array)):
+          smallest=Smallest_element(array)
+          New_array.append(array.pop(smallest))
+     return New_array
+
+     
+            
+            
+array=[2,3,4,5,6,7]
+Result=Smallest_element(array)
+selectionSort(array)
+print(Result)
