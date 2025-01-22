3c735ad01f6e5336131c67a3e67f05f95564d273
Yadnesh Madhukar Rane <102983422+Poneglyph795@users.noreply.github.com>
2025-01-22 14:37:17 +0530

    Add files via upload

---
+++ Add_number_in _array.py
@@ -0,0 +1,33 @@
+def Sum_Array(Array1):
+    first = Array1[0]
+    second = Array1[1]
+    result=first+second
+    return  result
+
+def Sum_Array2(Array2):
+    first = Array2[1]
+    second = Array2[2]
+    result1=first+second
+    return result1
+
+def Sum_Array3(Array3):
+    first = Array3[0]
+    second = Array3[1]
+    result2=first+second
+    return result2
+
+
+Array1=[2,7,11,15]
+Array2=[3,2,4]
+Array3=[3,3]
+
+First_result=Sum_Array(Array1)
+print(First_result)
+
+Second_result=Sum_Array2(Array2)
+print(Second_result)
+
+Third_result=Sum_Array3(Array3)
+print(Third_result)
+
+
