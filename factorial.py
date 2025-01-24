d5a1879479a480af00abdf2f9ff4d410dc08a849
Yadnesh_Rane <102983422+Poneglyph795@users.noreply.github.com>
2025-01-24 14:52:14 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,19 +1,17 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-             array=[0]*n
+             array=[0]*n                 ##  Making  array size of n                          
              index=0
 
              for i in range(1,n+1):
-                      if(n%i)==0:
-                        array[index]=i
-                        index+=1
-             if(k>len(array)):
+                      if(n%i)==0:         ## n divide i is equal to zero 
+                        array[index]=i    ## array.append[i]=i is  could be used here also
+                        index+=1          
+             if(k>len(array)):   
                 return -1  
              else:
               result=array[k]-1
-              return result
-    
-            
+              return result            
 
 solution=Solution()
 n=12
